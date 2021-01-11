9cd977682f1c413514346b52004028ca8b4c09e7
ductuan15 <47861874+ductuan15@users.noreply.github.com>
2021-01-11 17:48:15 +0700

    add screen memerbership detail ( chưa có chuyển cảnh)

---
+++ src/Controller/BIllPanel_controller.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import GUI.MainScreen.BillPanel;
+
+public class BIllPanel_controller {
+    BillPanel billPanel;
+    public BIllPanel_controller(BillPanel billPanel){
+        this.billPanel  = billPanel;
+
+    }
+}
